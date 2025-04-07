3d3e07e240e91678cdbb0968b3a60d82169925fe
Ali Hinnawe <hinnaweali@gmail.com>
2025-04-07 15:46:23 +0200

    adding cms project sample

---
+++ day16-cmsProject/cms_mit_DB/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/inc/all.php';
+
+$route = @(string) ($_GET['route'] ?? 'page');
+
+if ($route === 'page') {
+    // var_dump("hier die Startseite ...");
+    $pagesController = new \App\Controller\PagesController(
+        new \App\Pages\PagesRepository()
+    );
+    $page = @(string) ($_GET['page'] ?? 'index');
+    $pagesController->showPage($page);
+}
+else {
+    // var_dump("Hier die Fehlerseite (Error 404)");
+
+    $notFoundController = new \App\Controller\NotFoundController();
+    $notFoundController->error404();
+}

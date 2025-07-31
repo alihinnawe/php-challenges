5920e0ceab27faa48a6e9b81d844f4575263270d
Ali Hinnawe <hinnaweali@gmail.com>
2025-07-31 17:18:20 +0200

    adding more functions such as update, delete and insert all jokes

--- MorephpProjects/websites/default/public/jokes.php
+++ MorephpProjects/websites/default/public/jokes.php
@@ -1,18 +1,15 @@
  <?php
- try {
+try {
  include __DIR__ . '/../includes/DatabaseConnection.php';
  include __DIR__ . '/../includes/DatabaseFunctions.php';
- $sql = 'SELECT `joke`.`id`, `joketext`, `name`, `email`
- FROM `joke` INNER JOIN `author`
- ON `authorid` = `author`.`id`';
- $jokes = $pdo->query($sql);
-
+ $jokes = allJokes($pdo);
  $title = 'Joke list';
  $totalJokes = totalJokes($pdo);
  ob_start();
  include  __DIR__ . '/../templates/jokes.html.php';
  $output = ob_get_clean();
- } catch (PDOException $e) {
+ }
+ catch (PDOException $e) {
  $title = 'An error has occurred';
  $output = 'Database error: ' . $e->getMessage() . ' in ' .
  $e->getFile() . ':' . $e->getLine();
